bf0a34a767aea5676757e3f9f085dd88033894fc
Viihcerq <vitoriacerqsantos@gmail.com>
2025-02-20 22:50:15 -0300

    add new implementation

---
+++ src/main/java/com/github/viihcerq/study_apir/ControllerProduto.java
@@ -0,0 +1,26 @@
+package com.github.viihcerq.study_apir;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("produto")
+public class ControllerProduto {
+    @PostMapping
+    public ResponseEntity<String> create(){
+        return ResponseEntity.status(201).body("Produto criado");
+    }
+    @PutMapping
+    public ResponseEntity<String> update(){
+        return ResponseEntity.status(200).body("produto atualizado");
+    }
+
+    @GetMapping
+    public ResponseEntity<String> find(){
+        return ResponseEntity.status(200).body("Melancia");
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> delete(){
+        return ResponseEntity.status(204).build();
+    }
+}
