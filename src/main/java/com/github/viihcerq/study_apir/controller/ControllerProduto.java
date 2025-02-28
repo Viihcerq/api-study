be9ca0e93c01c938d7dd2a700948fc8896e6a578
Viihcerq <vitoriacerqsantos@gmail.com>
2025-02-27 21:02:07 -0300

    refactor class and create model product

--- src/main/java/com/github/viihcerq/study_apir/controller/ControllerProduto.java
+++ src/main/java/com/github/viihcerq/study_apir/controller/ControllerProduto.java
@@ -1,4 +1,4 @@
-package com.github.viihcerq.study_apir;
+package com.github.viihcerq.study_apir.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
