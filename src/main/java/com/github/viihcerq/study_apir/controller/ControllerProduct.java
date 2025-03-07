bb23b8a608f2230babebb90cdc30872dd5a02f29
Viihcerq <vitoriacerqsantos@gmail.com>
2025-03-06 22:53:54 -0300

    implementing method put

--- src/main/java/com/github/viihcerq/study_apir/controller/ControllerProduct.java
+++ src/main/java/com/github/viihcerq/study_apir/controller/ControllerProduct.java
@@ -6,21 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("product")
 public class ControllerProduct {
     @Autowired
     private ProductService service;
     @PostMapping
-    public ResponseEntity<String> create(Product product){
-        service.createProduct(null);
-        return ResponseEntity.status(201).body("Produto criado");
+    public ResponseEntity<Product> create(@RequestBody Product product){
+        Product productCreate = service.createProduct(product);
+        return ResponseEntity.status(201).body(productCreate);
     }
 
-    @PutMapping
-    public ResponseEntity<String> update(){
-        service.updateProduct(null, null);
-        return ResponseEntity.status(200).body("produto atualizado");
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<Product>> update(@PathVariable Long id, @RequestBody Product product){
+        Optional<Product> productUpdate = service.updateProduct(id, product);
+        return ResponseEntity.status(200).body(productUpdate);
     }
 
     @GetMapping
--- src/main/java/com/github/viihcerq/study_apir/service/ProductService.java
+++ src/main/java/com/github/viihcerq/study_apir/service/ProductService.java
@@ -3,15 +3,27 @@ package com.github.viihcerq.study_apir.service;
 import com.github.viihcerq.study_apir.model.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProductService {
-    private List<Product> products;
+    private List<Product> products = new ArrayList<>();
 
+    private Long sequence = 1L;
+    // o objeto é referenciado são os mesmos em memoria
     public Product createProduct(Product product){
-        return null;
+        product.setId(sequence++);
+        products.add(product);
+        return product;
     }
-    public void updateProduct(Long id, Product product){
+    public Optional<Product> updateProduct(Long id, Product product){
+        return products.stream().filter(product1 -> product1.getId().equals(id)).findFirst()
+                .map(p -> {
+                    p.setName(product.getName());
+                    return p;
+                });
 
     }
     public void getProductId(Long id){
